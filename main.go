ccabbf1617c5740759a4d7cc686d9d1b958b0753
Dmitry Pryshchepa <dmitry.pryshchepa@gmail.com>
2017-02-12 00:21:59 +0300

    make a couple small improvements

--- main.go
+++ main.go
@@ -1,28 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
 
-var routes []string
-
-type customRouter struct {
+func logger(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s requested %s", r.RemoteAddr, r.URL)
+		h.ServeHTTP(w, r)
+	})
 }
 
-func (customRouter) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	time.Sleep(time.Second * 1)
-	rw.WriteHeader(http.StatusAccepted)
+func setRoutes(h *http.ServeMux) {
+
+	h.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, you hit foo!")
+	})
+
+	h.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, you hit bar!")
+	})
+
+	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+		fmt.Fprintln(w, "You're lost, go home")
+	})
 }
 
 func main() {
-	var cr customRouter
+
+	h := http.NewServeMux()
+
+	setRoutes(h)
+	l:= logger(h)
+
 	server := &http.Server{
 		Addr:           ":9000",
-		Handler:        cr,
+		Handler:        l,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
+
 	server.ListenAndServe()
 }
+
